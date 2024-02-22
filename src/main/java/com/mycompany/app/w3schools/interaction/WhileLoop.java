ebf5735e6bd1370e55cb7c5b8486b746ada85547
Nghiem Duc Viet <105410776+ndviet0303@users.noreply.github.com>
2024-02-22 11:07:17 +0000

    w3school whileloop

---
+++ src/main/java/com/mycompany/app/w3schools/interaction/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.w3schools.interaction;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+    }
+}
