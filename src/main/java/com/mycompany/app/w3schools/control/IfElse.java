5db2d2b2e201432d583e4a8cc2e60bc0667bf9fc
Nghiem Duc Viet <105410776+ndviet0303@users.noreply.github.com>
2024-02-22 11:05:53 +0000

    w3school if else

---
+++ src/main/java/com/mycompany/app/w3schools/control/IfElse.java
@@ -0,0 +1,15 @@
+package com.mycompany.app.w3schools.control;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int time = 22;
+        if (time < 10) {
+          System.out.println("Good morning.");
+        } else if (time < 18) {
+          System.out.println("Good day.");
+        } else {
+          System.out.println("Good evening.");
+        }
+        // Outputs "Good evening."
+    }
+}
