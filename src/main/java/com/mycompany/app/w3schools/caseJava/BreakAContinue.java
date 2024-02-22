4e009940d3b92bdb312b5d6cd6a05b0706d6a82d
Nghiem Duc Viet <105410776+ndviet0303@users.noreply.github.com>
2024-02-22 11:17:30 +0000

    w3school Break and continue

---
+++ src/main/java/com/mycompany/app/w3schools/caseJava/BreakAContinue.java
@@ -0,0 +1,28 @@
+package com.mycompany.app.w3schools.caseJava;
+
+public class BreakAContinue {
+    public static void main(String[] args) {
+        // break
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+            if (i == 4) {
+                break;
+            }
+        }
+
+        // continue
+
+        i = 0;
+        while (i < 10) {
+            if (i == 4) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
