28926b5dfcefafb295056d7a08484b80ccd35d05
Nghiem Duc Viet <105410776+ndviet0303@users.noreply.github.com>
2024-02-22 09:58:23 +0000

    Aliasing n.i = 15

---
+++ src/main/java/com/mycompany/app/PassObject.java
@@ -0,0 +1,12 @@
+public class PassObject {
+    static void f(Number m) {
+    m.i = 15;
+    }
+    public static void main(String[] args) {
+    Number n = new Number();
+    n.i = 14;
+    f(n); // what is n.i now?
+    System.out.println(n.i);
+    }
+
+   }
