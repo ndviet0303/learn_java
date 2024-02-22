117b1c4905dda11cb352209da91969ca6ecd464d
Nghiem Duc Viet <105410776+ndviet0303@users.noreply.github.com>
2024-02-22 11:09:49 +0000

    w3school forloop

---
+++ src/main/java/com/mycompany/app/w3schools/interaction/ForLoop.java
@@ -0,0 +1,9 @@
+package com.mycompany.app.w3schools.interaction;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
